450fde0729e200badc63a6c6464e2b6d2f446336
Kerigan <kerigancreighton@gmail.com>
2024-08-18 19:54:42 -0500

    Set default toolchain back to gnueabi

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CC="$(pwd)/vic-toolchain/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc" \
+CC="$(pwd)/vic-toolchain/arm-linux-gnueabi/bin/arm-linux-gnueabi-gcc" \
 CGO_LDFLAGS="-L$(pwd)/build -L$(pwd)/build/libjpeg-turbo/lib" \
 GOARM=7 \
 GOARCH=arm \
